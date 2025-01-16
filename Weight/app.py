ce7768fdfb55d88ce1902f9e287c62945e808c62
HelgiDm <alpert98@mail.ru>
2025-01-16 17:29:45 +0200

    Added GET /health

--- Weight/app.py
+++ Weight/app.py
@@ -12,5 +12,22 @@ DB_CONFIG = {
     'port': 3306
 }
 
+def get_db_connection():
+    return mysql.connector.connect(**DB_CONFIG)
+
+
+# Route to check if Database Server is available Connecting to Database
+@app.route('/health', methods=['GET'])
+def check_mysql():
+    try:
+        mconn = get_db_connection()
+        if mconn.is_connected():
+            mconn.close()  # Closing connection
+            return jsonify({"status": "OK", "message": "MySQL server is running"}), 200
+        
+    except Exception as e:
+        return jsonify({"status": "Failure", "message": str(e)}), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
