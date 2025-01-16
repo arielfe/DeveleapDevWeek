da112b380280676eb1f69c70e21681bf97a0b40a
Admin <admin@Admins-MacBook-Pro.local>
2025-01-16 12:27:03 +0200

    put function was implemnted

--- Billing/app/router.py
+++ Billing/app/router.py
@@ -4,17 +4,16 @@ from app.controller import create_provider
 # Create a blueprint for provider-related routes
 provider_routes = Blueprint("provider_routes", __name__)
 
-# @provider_routes.route("/provider", methods=["POST"])
-# def post_provider():
-#     """
-#     Route to create a new provider.
-#     """
-#     # Parse JSON data from the request
-#     data = request.json
-#     if not data or "name" not in data:
-#         return jsonify({"error": "Provider name is required"}), 400
+
+@provider_routes.route("/provider/<int:id>", methods=["PUT"])
+def update_provider(id):
+    """
+    Route to update an existing provider.
+    """
+    data = request.json
+    if not data:
+        return "Provider name is required", 400
     
-#     # Call the controller function to handle business logic
-#     provider_name = data["name"]
-#     response = create_provider(provider_name)
-#     return response
+    new_name = data.get("name")
+    response, status_code = update_provider_controller(id, new_name)
+    return response, status_code
