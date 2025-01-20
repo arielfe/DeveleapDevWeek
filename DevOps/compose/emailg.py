0c26cc530cae037fe4013c3f7bf1298edb0e0056
Ariel Fellous <ariel.fellous@gmail.com>
2025-01-20 07:25:00 +0200

    use mail env variables in compose send mail

--- DevOps/compose/emailg.py
+++ DevOps/compose/emailg.py
@@ -1,13 +1,17 @@
 import smtplib
+import os
+
+email_user = os.environ("EMAIL")
+email_pass = os.environ("EMAIL_PASS")
 # creates SMTP session
 s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security
 s.starttls()
 # Authentication
-s.login("devweekorange", "aqac pvqi oseg wjni ")
+s.login(email_user, email_pass)
 # message to be sent
 message = "Message_you_need_to_send"
 # sending the mail
-s.sendmail("devweekorange@gmail.com", "ariel.fellous@gmail.com", message)
+s.sendmail(email_user, "ariel.fellous@gmail.com", message)
 # terminating the session
 s.quit()
