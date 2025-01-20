58348fe3598d48a3485efff4c97b3ffbe9de738f
Ariel Fellous <ariel.fellous@gmail.com>
2025-01-20 15:22:56 +0200

    fix builder

--- DevOps/builder/entrypoint.sh
+++ DevOps/builder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh                                                                                                                                                      
-GIT_SSH_COMMAND="ssh -i /conf/id_ed25519_dev -o LogLevel=quiet -o StrictHostKeyChecking=no " git clone  git@github.com:arielfe/DeveleapDevWeek.git
-cd DeveleapDevWeek
-GIT_SSH_COMMAND="ssh -i /conf/id_ed25519_dev -o LogLevel=quiet -o StrictHostKeyChecking=no " git checkout $COMMIT
-cd DevOps/compose
-docker compose up --abort-on-container-exit
+GIT_SSH_COMMAND="ssh -i /conf/id_ed25519_dev -o LogLevel=quiet -o StrictHostKeyChecking=no " git clone  git@github.com:arielfe/DeveleapDevWeek.git             
+cd DeveleapDevWeek                                                                                                                                             
+GIT_SSH_COMMAND="ssh -i /conf/id_ed25519_dev -o LogLevel=quiet -o StrictHostKeyChecking=no " git checkout $COMMIT                                              
+cd DevOps/compose                                                                                                                                              
+docker compose up  --always-recreate-deps --abort-on-container-exit 
 
