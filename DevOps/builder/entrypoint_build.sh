38034b684c505e78a3624bade6f1737b7e7604eb
Ariel Fellous <ariel.fellous@gmail.com>
2025-01-21 19:18:32 +0200

    Fix builder script and settings type

--- DevOps/builder/entrypoint_build.sh
+++ DevOps/builder/entrypoint_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # read configuration file for compose file locations within the repository
 
 
@@ -13,34 +13,37 @@ REPO_ROOT=$(pwd)
 . ./DevOps/config/compose_targets.sh
 
 # execute each docker compose testing file
-cd $BILLING_TEST_COMPOSE_FOLDER
-pwd
-docker compose -f $BILLING_TEST_COMPOSE_FILE up
+#cd $BILLING_TEST_COMPOSE_FOLDER
+#docker compose -f $BILLING_TEST_COMPOSE_FILE up
+echo $REPO_ROOT
+echo $WEIGHT_TEST_COMPOSE_FOLDER
 cd $REPO_ROOT
 cd $WEIGHT_TEST_COMPOSE_FOLDER
+pwd
+echo "docker compose -f $WEIGHT_TEST_COMPOSE_FILE up"
 docker compose -f $WEIGHT_TEST_COMPOSE_FILE up
 
 # run build test script. the script will also return mail to the committer
-cd $REPO_ROOT
-cd DevOps/build_tests
-python build_test.py
+#cd $REPO_ROOT
+#cd DevOps/build_tests
+#python build_test.py
 
 # if this is main branch and tests successful, need to run deployment
-DEPLOY="NO"
-if [ $? -eq 0 && $MAIN_BRANCH -eq "YES" ]; then
-   DEPLOY="YES"
-fi
+#DEPLOY="NO"
+#if [ $? -eq 0 && $MAIN_BRANCH -eq "YES" ]; then
+#   DEPLOY="YES"
+#fi
 
 # drop all testing env 
-cd $BILLING_TEST_COMPOSE_FOLDER
-docker compose -v -f $BILLING_TEST_COMPOSE_FILE down
-cd $REPO_ROOT
-cd $WEIGHT_TEST_COMPOSE_FOLDER
-docker compose -v -f $WEIGHT_TEST_COMPOSE_FILE down
-cd $REPO_ROOT
+#cd $BILLING_TEST_COMPOSE_FOLDER
+#docker compose -v -f $BILLING_TEST_COMPOSE_FILE down
+#cd $REPO_ROOT
+#cd $WEIGHT_TEST_COMPOSE_FOLDER
+#docker compose -v -f $WEIGHT_TEST_COMPOSE_FILE down
+#cd $REPO_ROOT
 
 # deploy if needded
-if [ $DEPLOY -eq "YES" ]; then
+if [[ $DEPLOY == "YES" ]]; then
    cd $BILLING_DEPLOY_COMPOSE_FOLDER
    docker compose -f  $BILLING_DEPLOY_COMPOSE_FILE up -d
    cd $REPO_ROOT
