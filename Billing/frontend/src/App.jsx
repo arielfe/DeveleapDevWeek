a4c698a4cb827136e278c0a9770cdcf34ee234cc
Ishay Ezagouri <ishay7@gmail.com>
2025-01-21 09:36:08 +0100

    change title color ui

--- Billing/frontend/src/App.jsx
+++ Billing/frontend/src/App.jsx
@@ -6,10 +6,13 @@ import PostTruck from "./components/PostTruck";
 import UpdateTruck from "./components/UpdateTruck";
 import UploadRates from "./components/UploadRates";
 import DownloadRates from "./components/DownloadRates";
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">Dashboard</h1>
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-500">
+        Billing Dashboard
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {/* Render multiple DummyCard components */}
         <DummyCard />
