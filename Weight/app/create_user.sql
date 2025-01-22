3cc6255ed47a5df498ee11d228a6f25eb38f090e
Ariel Fellous <ariel.fellous@gmail.com>
2025-01-22 10:20:19 +0200

    remove Weight in main so weight branch can be merged with no conflicts

--- Weight/app/create_user.sql
+++
@@ -1,5 +0,0 @@
--- Drop user if exists and recreate
-DROP USER IF EXISTS 'user_weight'@'%';
-CREATE USER 'user_weight'@'%' IDENTIFIED BY 'bashisthebest';
-GRANT ALL PRIVILEGES ON weight.* TO 'user_weight'@'%';
-FLUSH PRIVILEGES;
