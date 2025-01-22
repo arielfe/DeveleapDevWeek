69aea9b0592f3b418610f5cc4a095ffad0a3b150
E-zech <eliyaz2024@gmail.com>
2025-01-22 12:13:49 +0200

    fix a tab

--- DevOps/build_tests/build_tests.py
+++ DevOps/build_tests/build_tests.py
@@ -2,16 +2,18 @@ import smtplib
 import requests
 import sys
 import os
+import time
+from email.message import EmailMessage
 
 
 # services
 services = {
-    "backend-weight":"http://localhost:5555/health",
-    "backend-billing":"http://localhost:5000/health",
-    "frontend billing":"http://localhost:8084/health"
+    "backend-weight":"http://127.0.0.1:8085/health",
+    "backend-billing":"http://127.0.0.1:5000/health",
+    "frontend billing":"http://127.0.0.1:8084/health"
     }
 
-commiter = os.environ("EMAIL")
+commiter = os.environ.get("EMAIL")
 
 # extract email data
 with open('/run/secrets/my_email', 'r') as file:
